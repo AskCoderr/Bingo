5d4cd4da9dfe1bb4d779db8eced7bafdd43f4609
broodingcolonel7coderr <rvisal79@gmail.com>
2025-12-27 18:45:51 +0530

    Refactor ComputerPlayer and add Player class

--- src/Bingo/ComputerPlayer.java
+++ src/Bingo/ComputerPlayer.java
@@ -10,4 +10,16 @@ public class ComputerPlayer {
     private void decrementPoint() {
         --this.points;
     }
+    public class Player {
+        int completedLines = 0;
+
+        public void incrementLine() {
+            completedLines++;
+        }
+
+        public boolean hasBingo() {
+            return completedLines >= 5;
+        }
+    }
+
 }
