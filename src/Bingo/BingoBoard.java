28d8b94d20e37af769d3d4d223d84f45cd21922e
broodingcolonel7coderr <rvisal79@gmail.com>
2025-12-27 09:15:27 +0530

    Modify BingoBoard to accept variable size
    Refactor BingoBoard to support dynamic board size.

--- src/Bingo/BingoBoard.java
+++ src/Bingo/BingoBoard.java
@@ -3,46 +3,24 @@ package Bingo;
 import javax.swing.JButton;
 
 public class BingoBoard {
-	JButton[][] buttons = new JButton[5][5];
-	public int[][] board = new int[5][5];
-	public Node[][] nodes = new Node[5][5];
+    int size;
+    public JButton[][] buttons;
+    public int[][] board;
+    public Node[][] nodes;
 
-	public BingoBoard() {
-		createBoard();
-		connectNodes();
-	}
+    public BingoBoard(int size) {
+        this.size = size;
+        buttons = new JButton[size][size];
+        board = new int[size][size];
+        nodes = new Node[size][size];
 
-	private void createBoard() {
-		int num = 1;
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				board[i][j] = num;
-				nodes[i][j] = new Node(num++);
-			}
-		}
-	}
-
-	private void connectNodes() {
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-				if (i > 0)
-					nodes[i][j].neighbors.add(nodes[i - 1][j]);
-				if (i < 4)
-					nodes[i][j].neighbors.add(nodes[i + 1][j]);
-				if (j > 0)
-					nodes[i][j].neighbors.add(nodes[i][j - 1]);
-				if (j < 4)
-					nodes[i][j].neighbors.add(nodes[i][j + 1]);
-				if (i > 0 && j > 0)
-					nodes[i][j].neighbors.add(nodes[i - 1][j - 1]);
-				if (i > 0 && j < 4)
-					nodes[i][j].neighbors.add(nodes[i - 1][j + 1]);
-				if (i < 4 && j > 0)
-					nodes[i][j].neighbors.add(nodes[i + 1][j - 1]);
-				if (i < 4 && j < 4)
-					nodes[i][j].neighbors.add(nodes[i + 1][j + 1]);
-			}
-		}
-	}
-
+        int num = 1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = num++;
+                nodes[i][j] = new Node(board[i][j]);
+            }
+        }
+    }
 }
+
