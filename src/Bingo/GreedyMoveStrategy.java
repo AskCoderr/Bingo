732cf1ab4df26c7bff975d96696c52c869add5f9
broodingcolonel7coderr <rvisal79@gmail.com>
2026-02-14 12:55:53 +0530

    Update GreedyMoveStrategy.java
    Divide and Conquer strategy has been used as a result the computation of rowsum, colsum annol has been made simpler coz of them been broken down into smaller sub problems

--- src/Bingo/GreedyMoveStrategy.java
+++ src/Bingo/GreedyMoveStrategy.java
@@ -1,100 +1,92 @@
 package Bingo;
-
 public class GreedyMoveStrategy {
-    private final int SIZE = 5;
-
     public int findBestMove(BingoBoard compBoard){
-        int bestScore = -1;
-        int bestNumber = -1;
-
-        for(int i=0; i<SIZE; i++){
-            for(int j=0; j<SIZE; j++){
-                Node currentNode = compBoard.nodes[i][j];
-
-                if(!currentNode.marked){
-                    currentNode.marked = true;
-                    int score = evaluateBoard(compBoard);
-                    currentNode.marked = false;
-
-                    if(score > bestScore){
-                        bestScore = score;
-                        bestNumber = currentNode.value;
-                    }
-                }
-            }
-        }
-        return bestNumber;
+        int size = compBoard.size;
+        Result res = D_C(compBoard, 0, 0,size-1,size-1);
+        return res.number;
     }
-
-    private int evaluateBoard(BingoBoard b){
-        int score = 0;
-
-        // Rows and Columns
-        for(int i=0; i<SIZE; i++){
-            score += countRows(b,i);
-            score += countColumns(b,i);
+    private static class Result{
+        int score;
+        int number;
+        Result(int score, int number){
+            this.score = score;
+            this.number = number;
         }
-
-        score += countDiagonals(b);
-
+    }
+    private Result D_C(BingoBoard board , int r1, int c1, int r2, int c2){
+        if(r1==r2&&c1==c2){
+            Node node = board.nodes[r1][c1];
+            if (node.marked){
+                return new Result(-1,-1);
+            }
+            node.marked = true;
+            int score = EvaluateBoard(board);
+            node.marked = false;
+            return new Result(score, node.value);
+        }
+        int rm = (r1+r2)/2;
+        int cm = (c1+c2)/2;
+        Result best = new Result(Integer.MIN_VALUE,-1);
+        best = max(best, D_C(board, r1,c1,rm,cm));
+        if(cm+1<=c2){
+            best = max(best,D_C(board, r1, cm+1, rm,c2));
+        }
+        if(rm+1<=r2){
+             best = max(best,D_C(board, rm + 1, c1, r2, cm));
+        }
+        if(rm+1<=r2&&cm+1<=c2){
+             best = max(best,D_C(board, rm + 1, cm + 1, r2, c2));
+        }
+        return best;
+    }
+    private Result max(Result a , Result b ){
+        return b.score>a.score?b:a;
+    }
+    private int EvaluateBoard(BingoBoard b){
+        int score = 0;
+        int SIZE = b.size;
+        for(int i = 0;i<SIZE;i++){
+            score+=countRows(b,i);
+            score+=countColumns(b,i);
+        }
+        score+=countDiagonals(b);
         return score;
     }
-
-    private int countRows(BingoBoard b, int index){
+      private int countRows(BingoBoard b, int index) {
+        int SIZE = b.size;
         int count = 0;
-
-        for(int k=0; k<SIZE; k++){
-            if(b.nodes[index][k].marked){
-                count++;
-            }
+        for (int k = 0; k <SIZE; k++) {
+            if (b.nodes[index][k].marked) count++;
         }
-
-        if(count == SIZE){
-            return 100;
-        }
-        else{
-            return count;
-        }
+        return (count == SIZE) ?100 : count;
     }
-
-    private int countColumns(BingoBoard b, int index){
+    private int countColumns(BingoBoard b , int index){
+        int SIZE = b.size;
         int count = 0;
-
-        for(int k=0; k<SIZE; k++){
-            if(b.nodes[k][index].marked){
-                count++;
-            }
+        for (int k = 0; k < SIZE; k++) {
+            if (b.nodes[k][index].marked) count++;
         }
-
-        if(count == SIZE){
-            return 100;
-        }
-        else{
-            return count;
-        }
+        return (count == SIZE) ? 100 : count;
     }
-
     private int countDiagonals(BingoBoard b){
-        int d1=0, d2=0;
-        int score;
-
-        for(int i=0; i<SIZE; i++){
-            if(b.nodes[i][i].marked) d1++;
-            if(b.nodes[i][SIZE-i-1].marked) d2++;
+        int SIZE = b.size;
+        int d1 = 0;
+        int d2 = 0;
+        for(int i = 0;i<SIZE;i++){
+            if(b.nodes[i][i].marked){
+                d1++;
+            }
+            if (b.nodes[i][SIZE - i - 1].marked) {
+                d2++;
+            }
         }
-
-        if(d1 == SIZE){
-            score = 100;
-        }
-        else{
-            score = d1;
-        }
-        if(d2 == SIZE){
-            score += 100;
-        }
-        else{
-            score += d2;
-        }
+        int score = (d1 == SIZE ? 100 : d1);
+        score += (d2 == SIZE ? 100 : d2);
         return score;
     }
 }
+
+            
+
+        
+    
